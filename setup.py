da4a04ace794155a2f9b450beed01d734c5f7037
harini2923A <harini2923@gmail.com>
2026-03-04 08:54:00 -0500

    Requirments added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Meditalk",
+    version="0.1.0",
+    author="Harini",
+    author_email="hanakala29@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
